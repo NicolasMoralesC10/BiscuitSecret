73406b05d6f47e77b030b674b836568e019fb091
Morales <nicolaschompi@gmail.com>
2025-04-10 07:57:24 -0500

    Acoplacion de modulos

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -2,8 +2,8 @@
   <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
     <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ route('dashboard') }}">
-      <img src=" {{ asset( '../assets/img/logo-ct.png') }}" class="navbar-brand-img h-100" alt="...">
-      <span class="ms-3 font-weight-bold">Soft UI Dashboard Laravel</span>
+      <img src=" {{ asset( '../assets/img/logo-preview.png') }}" class="navbar-brand-img h-100" alt="...">
+      <span class="ms-3 font-weight-bold">Biscuit Secret</span>
     </a>
   </div>
   <hr class="horizontal dark mt-0">
@@ -54,7 +54,7 @@
           <span class="nav-link-text ms-1">Ventas</span>
         </a>
       </li>
-     <!--  <a class="nav-link {{ (Request::is('user-management') ? 'active' : '') }}" href="{{ url('user-management') }}">
+      <!--  <a class="nav-link {{ (Request::is('user-management') ? 'active' : '') }}" href="{{ url('user-management') }}">
         <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
           <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center text-dark {{ (Request::is('user-management') ? 'text-white' : 'text-dark') }} " aria-hidden="true"></i>
         </div>
@@ -76,10 +76,10 @@
           <span class="nav-link-text ms-1">Productos</span>
         </a>
       </li>
-     <!--  <li class="nav-item mt-2">
+      <!--  <li class="nav-item mt-2">
         <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Example pages</h6>
       </li> -->
-     <!--  <li class="nav-item">
+      <!--  <li class="nav-item">
         <a class="nav-link {{ (Request::is('tables') ? 'active' : '') }}" href="{{ url('tables') }}">
           <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
             <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -99,7 +99,7 @@
           <span class="nav-link-text ms-1">Tables</span>
         </a>
       </li> -->
-     <!--  <li class="nav-item">
+      <!--  <li class="nav-item">
         <a class="nav-link {{ (Request::is('billing') ? 'active' : '') }}" href="{{ url('billing') }}">
           <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
             <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
