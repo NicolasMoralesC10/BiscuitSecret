bb6544b018ec407b9b4ed2ebd7ad832ddadc8a42
Morales <nicolaschompi@gmail.com>
2025-04-10 08:13:41 -0500

    Acoplacion de modulos

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,3 +14,33 @@ function confirmDeleteUser(id, name) {
         }
     });
 }
+
+function confirmDelete(id) {
+    Swal.fire({
+        title: "¿Estás seguro?",
+        text: "¡Este producto será eliminado permanentemente!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#7066e0",
+        cancelButtonColor: "#6e7881",
+        cancelButtonText: "Cancelar",
+        confirmButtonText: "Sí, eliminar",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.getElementById("delete-form-" + id).submit();
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imageModal = document.getElementById("imageModal");
+    imageModal.addEventListener("show.bs.modal", function (event) {
+        var button = event.relatedTarget;
+        var imageUrl = button.getAttribute("data-img-url");
+        var imageLabel = button.getAttribute("data-title-img");
+        var imageModalLabel = document.getElementById("imageModalLabel");
+        var modalImage = document.getElementById("modalImage");
+        modalImage.src = imageUrl;
+        imageModalLabel.innerHTML = imageLabel;
+    });
+});
--- resources/views/layouts/footers/auth/footer.blade.php
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -1,36 +1,4 @@
 <footer class="footer pt-3  ">
-<!-- 
-    <script>
-        function confirmDelete(id) {
-            Swal.fire({
-                title: '¿Estás seguro?',
-                text: "¡Este producto será eliminado permanentemente!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: "#7066e0",
-                cancelButtonColor: "#6e7881",
-                cancelButtonText: 'Cancelar',
-                confirmButtonText: 'Sí, eliminar',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    document.getElementById('delete-form-' + id).submit();
-                }
-            });
-        }
-
-        document.addEventListener("DOMContentLoaded", function() {
-            var imageModal = document.getElementById('imageModal');
-            imageModal.addEventListener('show.bs.modal', function(event) {
-                var button = event.relatedTarget;
-                var imageUrl = button.getAttribute('data-img-url');
-                var imageLabel = button.getAttribute('data-title-img')
-                var imageModalLabel = document.getElementById('imageModalLabel')
-                var modalImage = document.getElementById('modalImage');
-                modalImage.src = imageUrl;
-                imageModalLabel.innerHTML = imageLabel;
-            });
-        });
-    </script> -->
     <div class="container-fluid">
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
